c604d1863c6e734f13214a484339fa8dd0204b6c
Blaise Schaeffer <bschaeffer@fshealth.com>
2025-04-20 12:08:53 -0500

    chore: node thpes and __dirname for paths

--- eslint.config.ts
+++ eslint.config.ts
@@ -41,7 +41,7 @@ export default tseslint.config(
       globals: globals.browser,
       parserOptions: {
         project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: './',
+        tsconfigRootDir: __dirname,
       },
     },
     plugins: {
